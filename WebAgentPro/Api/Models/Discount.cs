8cfc01373123e5929324fdef97c6c53438829d6e
Kris Horrocks <45402725+krishorrocks@users.noreply.github.com>
2020-10-24 11:57:31 -0700

    Made Discounts properties required

--- WebAgentPro/Api/Models/Discount.cs
+++ WebAgentPro/Api/Models/Discount.cs
@@ -11,23 +11,41 @@ namespace WebAgentPro.Api.Models
         [Key]
         [MinLength(2)]
         [MaxLength(2)]
+        [Required]
         public string State { get; set; }
+        [Required]
         public decimal DaytimeRunningLights { get; set; }
+        [Required]
         public decimal AntilockBrakes { get; set; }
+        [Required]
         public decimal LowAnnualMileage { get; set; }
+        [Required]
         public decimal PassiveRestraints { get; set; }
+        [Required]
         public decimal AntitheftInstalled { get; set; }
+        [Required]
         public decimal HighDaysDrivenPerWeek { get; set; }
+        [Required]
         public decimal LowMilesDrivenToWork { get; set; }
+        [Required]
         public decimal ReduceUse { get; set; }
+        [Required]
         public decimal GarageAddressDifferent { get; set; }
+        [Required]
         public decimal LowDrivingExperience { get; set; }
+        [Required]
         public decimal PreviousCarrierLizard { get; set; }
+        [Required]
         public decimal PreviousCarrierPervasive { get; set; }
+        [Required]
         public decimal RecentMovingViolations { get; set; }
+        [Required]
         public decimal RecentClaims { get; set; }
+        [Required]
         public decimal MultiCar { get; set; }
+        [Required]
         public decimal YoungDriver { get; set; }
+        [Required]
         public decimal SafeDrivingSchool { get; set; }
     }
 }
