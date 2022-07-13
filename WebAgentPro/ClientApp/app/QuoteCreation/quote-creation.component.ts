c2e12b37b37de1d6ee548a433c60738347f93424
Shubha Joshi <shubha.joshi@avanade.com>
2022-07-13 10:23:17 -0500

    incomplete page 1 design

--- WebAgentPro/ClientApp/app/QuoteCreation/quote-creation.component.ts
+++ WebAgentPro/ClientApp/app/QuoteCreation/quote-creation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-quote-creation',
@@ -9,9 +10,38 @@ export class QuoteCreationComponent implements OnInit {
 
   step: any = 1;
 
-  constructor() { }
+  public userForm: FormGroup;
+  first_name: string = "";
+  last_name: string = "";
+  address: string = "";
+  city: string = "";
+  state: string = "";
+  zip: number = 0;
+  ssn: number = 0;
+  dob: string = "";
+
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      first_name: "",
+      last_name: "",
+      address:"",
+      city:"",
+      state:"",
+      zip:0,
+      ssn:0,
+      dob:""
+    })
+  }
 
   submit(){
+    this.first_name = this.userForm.get("first_name")?.value;
+    this.last_name = this.userForm.get("last_name")?.value;
+    this.address = this.userForm.get("address")?.value;
+    this.city = this.userForm.get("city")?.value;
+    this.zip = this.userForm.get("zip")?.value;
+    this.ssn = this.userForm.get("ssn")?.value;
+    this.dob = this.userForm.get("dob")?.value;
+    console.log(this.first_name)
   }
 
   previous(){
