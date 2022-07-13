eaa7d0f5ef2a9313bcdd1f6991d63a019778ea18
Shubha Joshi <shubha.joshi@avanade.com>
2022-07-13 16:16:37 -0500

    page 1, 3, 5 mostly complete | started on 6

--- WebAgentPro/ClientApp/app/QuoteCreation/quote-creation.component.ts
+++ WebAgentPro/ClientApp/app/QuoteCreation/quote-creation.component.ts
@@ -15,48 +15,55 @@ import { Subscription } from 'rxjs';
 
 export class QuoteCreationComponent implements OnInit {
 
-    apiUrl: string = environment.apiUrl
-    quoteParamSubscription: Subscription
-    quote: Quote
-    action: string
-    step: any = 1;
-
-
+  apiUrl: string = environment.apiUrl
+  quoteParamSubscription: Subscription
+  quote: Quote
+  action: string
+  step: any = 6;
+  
   public userForm: FormGroup;
+  get firstName(){ return this.userForm.get('firstName'); }
+  get lastName(){ return this.userForm.get('lastName'); }
+  get address(){ return this.userForm.get('address'); }
+  get city(){ return this.userForm.get('city'); }
+  get state(){ return this.userForm.get('state'); }
+  get zip(){ return this.userForm.get('zip'); }
+  get ssn(){ return this.userForm.get('ssn'); }
+  get dateOfBirth(){ return this.userForm.get('dateOfBirth'); }
+
+  constructor(private route: ActivatedRoute, private http: HttpClient, private router: Router, private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.quoteParamSubscription = this.route.params.subscribe(
+      params => {
+          this.action = params['action']
 
 
-      constructor(private route: ActivatedRoute, private http: HttpClient, private router: Router, private fb: FormBuilder) { }
+          switch (this.action) {
+              case "add":
+                  this.initializeQuote(params['quoteId'])
+                  break
+              case "edit":
+                  this.getQuote(params['quoteId'])
+                  break
+          }
+      })
 
-    ngOnInit(): void {
-        this.quoteParamSubscription = this.route.params.subscribe(
-            params => {
-                this.action = params['action']
+      this.userForm = this.fb.group({
+        firstName: ['', [Validators.required, Validators.minLength(2)]],
+        lastName: ['', [Validators.required, Validators.minLength(2)]],
+        address: ['', [Validators.required, Validators.minLength(2)]],
+        city: ['', [Validators.required, Validators.minLength(2)]],
+        state: ['', [Validators.required]],
+        zip: [null, [Validators.required, Validators.pattern("^[0-9]*$")]],
+        ssn: [null, [Validators.required, Validators.pattern("^[0-9]*$")]],
+        dateOfBirth: [null, [Validators.required, Validators.pattern("^[0-9]*$")]],
+        safeDrivingSchool: [false, [Validators.requiredTrue,]]
+      })
 
-
-                switch (this.action) {
-                    case "add":
-                        this.initializeQuote(params['quoteId'])
-                        break
-                    case "edit":
-                        this.getQuote(params['quoteId'])
-                        break
-                }
-            }
-        )
-
-        this.userForm = this.fb.group({
-            first_name: ['', [Validators.required, Validators.minLength(2)]],
-            last_name: ['', [Validators.required, Validators.minLength(2)]],
-            address: ['', [Validators.required, Validators.minLength(2)]],
-            city: ['', [Validators.required, Validators.minLength(2)]],
-            state: ['', [Validators.required]],
-            zip: [null, [Validators.required, Validators.pattern("^[0-9]*$")]],
-            ssn: [null, [Validators.required, Validators.pattern("^[0-9]*$")]],
-            safeDrivingSchool: [false, [Validators.requiredTrue,]]
-        })
-
-        this.userForm.valueChanges.subscribe(console.log)
+      this.userForm.valueChanges.subscribe(console.log);
     }
+
     //This method would be called if you were to exit out early during
     //creation or editing. This will change when we have partial quotes
     //being saved and marked as uncompleted. 
@@ -65,66 +72,109 @@ export class QuoteCreationComponent implements OnInit {
     }
 
     submitForm() {
-        switch (this.action) {
-            case "add":
-                this.postQuote(this.quote)
-                break
-            case "edit":
-                this.putQuote(this.quote)
-                break
-        }
+      switch (this.action) {
+          case "add":
+              this.postQuote(this.quote);
+              break;
+          case "edit":
+              this.putQuote(this.quote);
+              break;
+      }
     }
 
     initializeQuote(quoteId: number) {
-        this.quote = new Quote
-        this.quote.quoteId = quoteId
+      this.quote = new Quote;
+      this.quote.quoteId = quoteId;
     }
 
     getQuote(quoteId: number) {
-        var httpRequest = this.http.get<Quote>(`${this.apiUrl}/quotes/${quoteId}`)
+      var httpRequest = this.http.get<Quote>(`${this.apiUrl}/quotes/${quoteId}`);
 
-        httpRequest.subscribe(
-            returnedQuote => {
-                this.quote = returnedQuote
-            }
-        )
+      httpRequest.subscribe(
+          returnedQuote => {
+              this.quote = returnedQuote
+          }
+      );
     }
 
     putQuote(updatedQuote: Quote) {
-        var httpRequest = this.http.put(`${this.apiUrl}/quotes/${updatedQuote.quoteId}`, updatedQuote)
+      var httpRequest = this.http.put(`${this.apiUrl}/quotes/${updatedQuote.quoteId}`, updatedQuote)
 
-        httpRequest.subscribe(
-            success => {
-                this.router.navigateByUrl("/quotes")
-            })
+      httpRequest.subscribe(
+          success => {
+              this.router.navigateByUrl("/quotes")
+          });
     }
 
     postQuote(newQuote: Quote) {
-        var httpRequest = this.http.post<number>(`${this.apiUrl}/quotes`, newQuote)
+      var httpRequest = this.http.post<number>(`${this.apiUrl}/quotes`, newQuote);
 
-        httpRequest.subscribe(
-            success => {
-                this.router.navigateByUrl("/quotes")
-            })
+      httpRequest.subscribe(
+          success => {
+              this.router.navigateByUrl("/quotes")
+          });
     }
 
 
     //These methods are used by the corresponding html to keep track
     //of the page on which quote creation resides
-    previous() {
-        this.step = this.step - 1;
-    }
+    previous() { this.step = this.step - 1; }
+    continue() { this.step = this.step + 1; }
 
-    continue() {
-        this.step = this.step + 1;
-    }
-
-  get first_name(){ return this.userForm.get('first_name'); }
-  get last_name(){ return this.userForm.get('last_name'); }
-  get address(){ return this.userForm.get('address'); }
-  get city(){ return this.userForm.get('city'); }
-  get state(){ return this.userForm.get('state'); }
-  get zip(){ return this.userForm.get('zip'); }
-  get ssn(){ return this.userForm.get('ssn'); }
-  get dob(){ return this.userForm.get('dob'); }
+    listOfStates: string[] = [
+      "Alaska",
+      "Alabama",
+      "Arkansas",
+      "American Samoa",
+      "Arizona",
+      "California",
+      "Colorado",
+      "Connecticut",
+      "District of Columbia",
+      "Delaware",
+      "Florida",
+      "Georgia",
+      "Guam",
+      "Hawaii",
+      "Iowa",
+      "Idaho",
+      "Illinois",
+      "Indiana",
+      "Kansas",
+      "Kentucky",
+      "Louisiana",
+      "Massachusetts",
+      "Maryland",
+      "Maine",
+      "Michigan",
+      "Minnesota",
+      "Missouri",
+      "Mississippi",
+      "Montana",
+      "North Carolina,",
+      "North Dakota",
+      "Nebraska",
+      "New Hampshire",
+      "New Jersey",
+      "New Mexico",
+      "Nevada",
+      "New York",
+      "Ohio",
+      "Oklahoma",
+      "Oregon",
+      "Pennsylvania",
+      "Puerto Rico",
+      "Rhode Island",
+      "South Carolina",
+      "South Dakota",
+      "Tennessee",
+      "Texas",
+      "Utah",
+      "Virginia",
+      "Virgin Islands",
+      "Vermont",
+      "Washington",
+      "Wisconsin",
+      "West Virginia",
+      "Wyoming"]
 }
